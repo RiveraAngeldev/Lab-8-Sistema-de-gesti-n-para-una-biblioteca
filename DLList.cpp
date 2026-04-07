5397e84227349538c9a8e9d9ba427119ea7a1f98
chrism18719-cmyk <chrism18719@gmail.com>
2026-04-07 15:35:17 -0400

    Started the implementation of the DLLits

--- DLList.cpp
+++ DLList.cpp
@@ -1,1 +1,81 @@
 #include "DLList.h"
+
+//Constructor
+DLList::DLList()
+{
+
+}
+
+//Deesctructor
+DLList::~DLList()
+{
+
+}
+
+//Basic Functions
+void DLList::addToHead(string, string)
+{
+
+}
+
+void DLList::addToTail(string, string)
+{
+
+}
+
+void DLList::deleteFromHead()
+{
+
+}
+
+void DLList::deleteFromTail()
+{
+
+}
+
+bool DLList::isEmpty()
+{
+
+}
+
+//Required Functions for the Lab
+void DLList::sortInsert(books*)
+{
+
+}
+void DLList:: deleteAllButTail()
+{
+
+}
+	void DLList::deleteAllButHead()
+    {
+
+    }
+	string DLList:: findBookByTitle(string)
+    {
+
+    }
+	void DLList:: deleteBookByTitle(string)
+    {
+
+    }
+	void DLList:: setBook(string, string, string)
+    {
+
+    }
+	void DLList::printBooksByAuthor(string)
+    {
+
+    }
+	void DLList:: showBooks()
+    {
+
+    }
+	void DLList:: deleteAllTheBooks()
+    {
+
+    }
+	int DLList:: obtainSize() const
+    {
+        
+    }
