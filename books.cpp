25d573d885a26435a1bca60de7e6015920e6189d
Rivera Angel <rivera.angeldev@gmail.com>
2026-04-07 15:00:05 -0400

    Add books class implementation with constructors, getters, and setters

---
+++ books.cpp
@@ -0,0 +1,47 @@
+#include "books.h"
+
+//Constructors
+books::books() {
+
+}
+
+books::books(const string &t, const string &a, books* n, books* p) {
+    books::title = t;
+    books::author = a;
+    books::next = n;
+    books::prev = p;
+}
+
+//Getters
+string books::getTitle() const {
+    return books::title;
+}
+
+string books::getAuthor() const {
+    return books::author;
+}
+
+books*::books::getNext() const {
+    return books::next;
+}
+
+books*::books::getPrev() const {
+    return books::prev;
+}
+
+//Setters
+void books::setTitle(string const& t) {
+    books::title = t;
+}
+
+void books::setAuthor(string const& a) {
+    books::author = a;
+}
+
+void books::setNext(books* n) {
+    books::next = n;
+}
+
+void books::setPrev(books* p) {
+    books::prev = p;
+}
